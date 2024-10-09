d6dade7a450a82a50aafbc9ab9af86fffc5ad337
Rookie_Zoe <11600873+RookieZoe@users.noreply.github.com>
2024-10-09 15:26:08 +0800

    [chore]: Initial commit

---
+++ .vscode/vite.config.proxy.ts
@@ -0,0 +1,12 @@
+export default {
+  port: 3010
+  // proxy: {
+  //   '/api': {
+  //     ws: false,
+  //     target: 'https://xxx/', //dev
+  //     // target: 'http://yyy:9300/', //sit
+  //     changeOrigin: true
+  //     // rewrite: (path: string) => path.replace(/^\/api/, '')
+  //   }
+  // }
+};
