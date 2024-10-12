e934069e05b01d3fcaa4152b1caa16d78a02dd52
rookiezoe <11600873+RookieZoe@users.noreply.github.com>
2024-10-12 17:55:07 +0800

    [chore]: Refact model loader

---
+++ src/components/model-factory.tsx
@@ -0,0 +1,86 @@
+import type { Mesh } from 'three';
+import { useGLTF } from '@react-three/drei';
+import { useMemo } from 'react';
+
+export enum UNIT {
+  m = 1,
+  dm = 0.1,
+  cm = 0.01,
+  mm = 0.001
+}
+
+type Props3D = JSX.IntrinsicElements['group'] & {
+  unit?: UNIT;
+  offset?: [number, number, number];
+  zReverse?: boolean;
+  xReverse?: boolean;
+  yReverse?: boolean;
+};
+
+export const get3DModel = (gltfPath: string) => {
+  useGLTF.preload(gltfPath);
+
+  // Set the bottom left rear vertex of the 3D model to the origin of the coordinate system,
+  // and add unit support to facilitate subsequent positioning.
+  return (props: Props3D) => {
+    const GLTF = useGLTF(gltfPath);
+
+    const {
+      unit = UNIT.m,
+      offset = [0, 0, 0],
+      zReverse = false,
+      xReverse = false,
+      yReverse = false,
+      ...rest
+    } = props;
+
+    const positionOffset = useMemo<[x: number, y: number, z: number]>(() => {
+      if (!GLTF || !GLTF.nodes) {
+        return [0, 0, 0];
+      }
+
+      let offset = [0, 0, 0];
+      let radius = 0;
+      Object.values(GLTF.nodes).forEach(meshNode => {
+        if (!meshNode || !(meshNode as Mesh).isMesh) {
+          return;
+        }
+        const r = (meshNode as Mesh)?.geometry?.boundingSphere?.radius || 0;
+        if (r > radius) {
+          radius = r;
+          const { x = 0, y = 0, z = 0 } = (meshNode as Mesh)?.geometry?.boundingBox?.max || {};
+          offset = [x, y, z];
+        }
+      });
+
+      return offset as [x: number, y: number, z: number];
+    }, [GLTF]);
+
+    return (
+      <group {...rest} scale={[unit, unit, unit]} dispose={null}>
+        <group scale={[1 / unit, 1 / unit, 1 / unit]} dispose={null} position={offset}>
+          {Object.values(GLTF.nodes).map((meshNode, index) => {
+            if (!meshNode || !(meshNode as Mesh).isMesh) {
+              return null;
+            }
+
+            return (
+              <mesh
+                key={index}
+                geometry={(meshNode as Mesh).geometry}
+                material={(meshNode as Mesh).material}
+                position={positionOffset}
+                receiveShadow
+                rotation={[
+                  xReverse ? Math.PI : 0,
+                  yReverse ? Math.PI : 0,
+                  zReverse ? Math.PI : 0
+                ]}
+              />
+            );
+          })}
+        </group>
+      </group>
+    );
+  };
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,51 @@
 import './global.less';
-import { StrictMode } from 'react';
+import { ReactNode, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import { CameraControls, ContactShadows, Environment } from '@react-three/drei';
+
 import NetworkCabinet from './components/network-cabinet';
+
+const World = (props: { children: ReactNode }) => {
+  return (
+    <Canvas
+      dpr={[1, 2]}
+      style={{ flex: '1' }}
+      eventSource={document.getElementById('app')!}
+      eventPrefix='client'
+      camera={{
+        fov: 8,
+        position: [6.6, 8.6, 6.2],
+        viewport: new THREE.Vector4(0, 6, 0, 0)
+      }}
+    >
+      <ambientLight intensity={0.2} />
+      <spotLight
+        intensity={0.1}
+        // angle={0.1}
+        penumbra={0.2}
+        position={[-10, 15, 10]}
+        distance={20}
+        castShadow
+      />
+      <Environment preset='studio' background blur={10} />
+      <CameraControls />
+      //here axes helper is applied
+      <primitive object={new THREE.AxesHelper(100)} />
+      <ContactShadows
+        resolution={512}
+        position={[0, 0, 0]}
+        opacity={1}
+        scale={10}
+        blur={2}
+        far={0.8}
+      />
+      {props.children}
+    </Canvas>
+  );
+};
 
 createRoot(document.getElementById('app')!).render(
   <StrictMode>
@@ -12,6 +55,8 @@ createRoot(document.getElementById('app')!).render(
       </h1>
       <h2>Still Working 🚧</h2>
     </div>
-    <NetworkCabinet />
+    <World>
+      <NetworkCabinet />
+    </World>
   </StrictMode>
 );
